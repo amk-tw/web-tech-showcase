34ca49a29f74da3d4905d5723083811a2c8635ea
Andrew M Krug <andrew.krug@thoughtworks.com>
2023-02-08 17:26:49 -0500

    fix(lint): 🐛 removed unneeded plugins

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,18 +2,10 @@ import { defineConfig } from "astro/config"
 
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
-// import compressor from "astro-compressor";
-// import compress from "astro-compress";
 import tailwind from "@astrojs/tailwind"
 
 export default defineConfig({
-  integrations: [
-    image(),
-    mdx(),
-    tailwind(),
-    // compress(),
-    // compressor()
-  ],
+  integrations: [image(), mdx(), tailwind()],
   site: "https://amk-tw.github.io",
   base: "/web-tech-showcase",
 })
